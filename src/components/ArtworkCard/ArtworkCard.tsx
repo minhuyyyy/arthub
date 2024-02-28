85060713b8d251ebf65040f5f02b488296411196
MinhHuy-SE170417 <huytran21a@gmail.com>
2024-02-28 11:39:36 +0700

    updated card

--- src/components/ArtworkCard/ArtworkCard.tsx
+++ src/components/ArtworkCard/ArtworkCard.tsx
@@ -4,6 +4,7 @@ import {
     CardActionArea,
     CardContent,
     CardMedia,
+    Grid,
     Typography,
 } from '@mui/material';
 import { shuffleImages } from '../../utils/utils';
@@ -13,38 +14,50 @@ function ArtworkCard() {
     // const shuffledCard: CardType[] = shuffleImages(FakeCard);
     // const [card, setCard] = useState(Card)
     // console.log(shuffledCard);
-
+    // const onScroll = () => {
+    //   const isEndPage =
+    // }
     return (
-        <Box>
-            <>
-                {FakeCard.map((card: CardType) => (
-                    <Card
-                        sx={{ minWidth: 345 }}
-                        key={card._id}>
-                        <CardActionArea>
-                            <CardMedia
-                                component='img'
-                                height='140px'
-                                image={card.imgLink}
-                                alt={card.imgDescription}
-                            />
-                            <CardContent>
-                                <Typography
-                                    gutterBottom
-                                    variant='h5'
-                                    component='div'>
-                                    {card.owner.name}
-                                </Typography>
-                                <Typography
-                                    variant='body2'
-                                    color='text.secondary'>
-                                    {card.imgDescription}
-                                </Typography>
-                            </CardContent>
-                        </CardActionArea>
-                    </Card>
-                ))}
-            </>
+        <Box sx={{marginTop:'20px'}}>
+            <Grid
+                container
+                spacing={1}>
+                <>
+                    {FakeCard.map((card: CardType) => (
+                        <Grid
+                            item
+                            sm={4}
+                            md={3}
+                            lg={3}>
+                            <Card
+                                sx={{ maxWidth: 345 }}
+                                key={card._id}>
+                                <CardActionArea>
+                                    <CardMedia
+                                        component='img'
+                                        height='140px'
+                                        image={card.imgLink}
+                                        alt={card.imgDescription}
+                                    />
+                                    <CardContent>
+                                        <Typography
+                                            gutterBottom
+                                            variant='h5'
+                                            component='div'>
+                                            {card.owner.name}
+                                        </Typography>
+                                        <Typography
+                                            variant='body2'
+                                            color='text.secondary'>
+                                            {card.imgDescription}
+                                        </Typography>
+                                    </CardContent>
+                                </CardActionArea>
+                            </Card>
+                        </Grid>
+                    ))}
+                </>
+            </Grid>
         </Box>
     );
 }
